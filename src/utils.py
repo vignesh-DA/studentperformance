59f44cb38793d7aa60b27a257cfdf38f7806ed7a
vignesh-DA <vigneshgogula9@gmail.com>
2025-12-17 14:18:55 +0530

    Model Training Done

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -16,3 +17,26 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
