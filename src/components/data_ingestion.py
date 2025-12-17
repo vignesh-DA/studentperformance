35f9249330c4b7598b3a130f4e61fb91114bb1e8
vignesh-DA <vigneshgogula9@gmail.com>
2025-12-17 16:10:08 +0530

    Hyper Parametric Tuning Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -56,5 +56,5 @@ if __name__=="__main__":
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     model_trainer=ModelTrainer()
-    model_trainer.initiate_model_trainer(train_arr,test_arr)
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
